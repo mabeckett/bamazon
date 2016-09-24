0bd7166a217ad59cb7a8ee066acdb6b5cc5eb989
Andrew Beckett <Andrew Beckett>
2016-09-23 20:57:39 -0400

    push

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -5,29 +5,32 @@ var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
-	password: '', //remember to enter your password
+	password: '#Gr33kbanah#', //remember to enter your password
 	database: 'bamazon'
 });
 // tests to confirm a connection
 connection.connect(function(error) {
 	if (error) throw error;
 	console.log('connected as id' + connection.threadId);
-	// start();
+	inventoryList();
 });
 
 var inventoryList = function() {
 	connection.query('SELECT * FROM products', function(error, products) {
 		if (error) throw error;
+
+		 for (var i=0; i < products.length; i++) {
+		 	console.log('-----------------------------------------------------------------------');
+		 	console.log('Item ID:' + products[i].ItemID + '/Product Name:' + products[i].ProductName + '/Department Name:' + products[i].DepartmentName + 
+		 		'/Price:' + products[i].Price);
+		 	console.log('-----------------------------------------------------------------------');
+		 };
 	});
 
-	 for (var i=0; i < prodcuts.length; i++) {
-	 	console.log('-----------------------------------------------------------------------');
-	 	console.log('Item ID: ' + products[i].ItemID + 'Product Name: ' + products[i].ProductName + 'Department Name: ' + products[i].DepartmentName + 
-	 		'Price: ' + products[i].Price);
-	 	console.log('-----------------------------------------------------------------------');
-	 };
+	
 
 
 
 
 }
+// inventoryList();
