79e7c9ac0394fe1e3bb92339da3bfcf98db406ec
Andrew Beckett <Andrew Beckett>
2016-09-27 18:25:22 -0400

    push

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -12,20 +12,36 @@ var connection = mysql.createConnection({
 connection.connect(function(error) {
 	if (error) throw error;
 	console.log('connected as id' + connection.threadId);
-	inventoryList();
+	inventoryList();//this starts the below 
 });
-
+//this pulls up my inventory list and shows the products in the console
 var inventoryList = function() {
 	connection.query('SELECT * FROM products', function(error, products) {
 		if (error) throw error;
 
-		 for (var i=0; i < products.length; i++) {
+		for (var i=0; i < products.length; i++) {
 		 	console.log('-----------------------------------------------------------------------');
 		 	console.log('Item ID:' + products[i].ItemID + '/Product Name:' + products[i].ProductName + '/Department Name:' + products[i].DepartmentName + 
 		 		'/Price:' + products[i].Price);
 		 	console.log('-----------------------------------------------------------------------');
 		 };
+		 purchaseOrder();
 	});
+
+var purchaseOrder = function() {
+	inquirer.prompt([
+	{
+		name: 'ItemID',
+		type: 'input',
+		message: 'Choose a product to purchase by entering the ItemID'
+	},
+	{
+		name: 'Quanity',
+		type: 'input',
+		message: 'Choose the quanity of your chosen product'
+	
+	}]);
+}
 
 	
 
